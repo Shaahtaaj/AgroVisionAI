af583f9ca30fbb5f4326b68dafb4792fe9d36e23
Shah Taj Memon <shoaib.gujjo@gmail.com>
2026-09-18 21:34:07 +0500

    fix(android): resolve Windows cross-drive path relativization error in Gradle and Kotlin

--- agrovision_ai/android/build.gradle.kts
+++ agrovision_ai/android/build.gradle.kts
@@ -16,8 +16,10 @@ val newBuildDir: Directory =
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    if (project.projectDir.toPath().root == newBuildDir.asFile.toPath().root) {
+        val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+        project.layout.buildDirectory.value(newSubprojectBuildDir)
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
